36151304d5748d34e55156d62f4b4e26761dcbcb
omertb <omertugrul@gmail.com>
2020-10-13 21:45:54 +0300

    logging, sent sms count

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 from forms import UserForm, SmsForm
 from random import randint
+from logger import send_wr_log
 import time
+from time import gmtime, strftime
 import os
 import ldap
 import requests
@@ -38,6 +40,17 @@ SMS_USER_PASS = os.environ['SMSUSERPASS']
 
 # disable certificate verification
 requests.packages.urllib3.disable_warnings()
+
+sms_error_dict = {
+    '1': "invalid credential",
+    '2': "account in debit",
+    '3': "invalid action element",
+    '5': "xml error",
+    '6': "invalid originator element",
+    '7': "message id not found",
+    '9': "invalid date",
+    '10': "sms not sent"
+}
 
 
 def generate_sms_code():
@@ -97,23 +110,47 @@ def verify_user(username, phone_number):
         return False
 
 
+def check_sms_count(phone_number):
+    DIR = "/project/sms_count/"
+    NAME_FORMAT = "%Y%m%d.log"
+    log_filename = "{}{}".format(DIR, strftime(NAME_FORMAT, gmtime()))
+    os.makedirs(os.path.dirname(log_filename), exist_ok=True)
+
+    with open(log_filename, "a") as f:
+        f.write("{}\n".format(phone_number))
+
+    with open(log_filename, "r") as f:
+        numbers_str = f.read()
+    numbers_list = numbers_str.splitlines()
+    if numbers_list.count(phone_number) > 5:
+        return False
+    else:
+        return True
+
+
 def send_sms(phone_number):
     sms_code = generate_sms_code()
     session['time_when_generated'] = int(time.time())
     session['sms_code_in_session'] = str(sms_code)
-    url = SMS_API_URL
-    user_pass_list = SMS_USER_PASS.split(',')
-
-    payload = "<SingleTextSMS> <UserName>{}</UserName> <PassWord>{}</PassWord> <Action>0</Action> " \
-              "<Mesgbody>OTP Reset/Unlock Code: {}</Mesgbody> <Numbers>{}</Numbers> " \
-              "</SingleTextSMS>".format(user_pass_list[0], user_pass_list[1], str(sms_code), phone_number)
-
-    response = requests.request("POST", url, data=payload)
-
-    if "ID" in response.text:
-        return True
-    else:
-        return False
+    return True
+#    url = SMS_API_URL
+#    user_pass_list = SMS_USER_PASS.split(',')
+#
+#    payload = "<SingleTextSMS> <UserName>{}</UserName> <PassWord>{}</PassWord> <Action>0</Action> " \
+#              "<Mesgbody>OTP Reset/Unlock Code: {}</Mesgbody> <Numbers>{}</Numbers> " \
+#              "</SingleTextSMS>".format(user_pass_list[0], user_pass_list[1], str(sms_code), phone_number)
+#
+#    response = requests.request("POST", url, data=payload)
+#
+#    if "ID" in response.text:
+#        message = "Sent SMS to phone number: {} with ID Number: {}".format(phone_number, response.text)
+#        send_wr_log(message)
+#        return True
+#    else:
+#        message = "Failed to send SMS to phone number: {}; Error Code: {}, {}".format(phone_number, response.text,
+#                                                                                      sms_error_dict[response.text])
+#        send_wr_log(message)
+#        return False
 
 
 def unlock_vpn_otp(username):
@@ -148,14 +185,33 @@ def reset_vpn_otp(username):
 def unlock():
     form = UserForm(request.form)
     if request.method == 'POST':
-        time.sleep(2)
 
+        time.sleep(2)
         if form.validate_on_submit():
             username = request.form['input_username']
+            username = username.split('@')[0]
+            username = username.split("\\")[-1]
             phone_number = request.form['input_phone_number']
 
-            user_is_verified = verify_user(username, phone_number)
+            try:
+                user_is_verified = verify_user(username, phone_number)
+            except ldap.LDAPError as e:
+                flash("LDAP Error: {}".format(e), "danger")
+                return render_template('unlock_form.html', form=form)
+            # logging:
+            message = "User: {} with phone number: {};" \
+                      " Verification : {}".format(username, phone_number,
+                                                  "SUCCESS" if user_is_verified else "FAIL")
+            send_wr_log(message)
+
             if user_is_verified:
+                below_limit = check_sms_count(phone_number)
+                if not below_limit:
+                    message = "You cannot send more than 5 SMS per day. Please contact to 1818"
+                    send_wr_log("User: {} - {}".format(username, message))
+                    flash(message, "danger")
+                    return render_template('unlock_form.html', form=form)
+
                 session['username'] = username
                 sms_is_sent = send_sms(phone_number)
                 if sms_is_sent:
@@ -177,10 +233,30 @@ def reset():
     if request.method == 'POST':
         if form.validate_on_submit():
             username = request.form['input_username']
+            username = username.split('@')[0]
+            username = username.split("\\")[-1]
             phone_number = request.form['input_phone_number']
 
-            user_is_verified = verify_user(username, phone_number)
+            try:
+                user_is_verified = verify_user(username, phone_number)
+            except ldap.LDAPError as e:
+                flash("LDAP Error: {}".format(e), "danger")
+                return render_template('reset_form.html', form=form)
+
+            # logging:
+            message = "User: {} with phone number: {};" \
+                      " Verification : {}".format(username, phone_number,
+                                                  "SUCCESS" if user_is_verified else "FAIL")
+            send_wr_log(message)
+
             if user_is_verified:
+                below_limit = check_sms_count(phone_number)
+                if not below_limit:
+                    message = "You cannot send more than 5 SMS per day. Please contact to 1818"
+                    send_wr_log("User: {} - {}".format(username, message))
+                    flash(message, "danger")
+                    return render_template('unlock_form.html', form=form)
+
                 session['username'] = username
                 sms_is_sent = send_sms(phone_number)
                 if sms_is_sent:
@@ -204,7 +280,9 @@ def sms_code_input():
                 if current_time - session['time_when_generated'] < 120:
                     sms_input_from_user = request.form['input_sms_code']
                     if sms_input_from_user == session['sms_code_in_session']:
-                        print("SUCCESS!")
+                        message = "User: {} - SMS Code Verification: SUCCESS".format(session['username'])
+                        send_wr_log(message)
+
                         if session['reset']:
                             vpn_api_call_result = reset_vpn_otp(session['username'])
                         else:
@@ -219,6 +297,8 @@ def sms_code_input():
                             flash_cat = "warning"
                         else:
                             flash_cat = "success"
+                        message = "User: {} - Pulse Secure Response: {}".format(session['username'], vpn_api_call_result)
+                        send_wr_log(message)
                         flash(vpn_api_call_result, flash_cat)
                         #
                         print("User input: {}".format(sms_input_from_user))
@@ -226,11 +306,15 @@ def sms_code_input():
                         session.pop('sms_code_in_session', None)
                     else:
                         flash('Wrong SMS Code!', "danger")
+                        message = "User: {} - SMS Verification: FAILED".format(session['username'])
+                        send_wr_log(message)
                         print("User input: {}".format(sms_input_from_user))
                         print("Generated: {}".format(session['sms_code_in_session']))
                         print("FAIL!")
                 else:
                     flash('Failure! SMS is no longer valid; returned previous page!', "danger")
+                    message = "User: {} - SMS Code Timeout".format(session['username'])
+                    send_wr_log(message)
                     session.pop('sms_code_in_session', None)
                     time.sleep(5)
                     if session['reset']:
---
+++ logger.py
@@ -0,0 +1,41 @@
+from logging.handlers import SysLogHandler
+import logging
+from time import gmtime, strftime
+import os
+# import socket
+
+
+LOG_SERVER = None
+DATE_FORMAT = "[%a, %d/%b/%Y %H:%M:%S +0000]"
+NAME_FORMAT = "%Y%m%d.log"
+LOG_DIR = "/project/logs/"
+
+
+class ContextFilter(logging.Filter):
+    # hostname = socket.gethostname()
+    hostname = "meranet-flask-server"
+
+    def filter(self, record):
+        record.hostname = ContextFilter.hostname
+        return True
+
+
+def send_wr_log(log_message):
+    if LOG_SERVER:
+        syslogger = logging.getLogger('SyslogLogger')
+        syslogger.setLevel(logging.INFO)
+        log_handler = SysLogHandler(address=(LOG_SERVER, 514))
+        log_handler.addFilter(ContextFilter())
+        msg_format = '%(asctime)s %(hostname)s  %(message)s'
+        formatter = logging.Formatter(msg_format, datefmt='%b %d %H:%M:%S')
+        log_handler.setFormatter(formatter)
+        syslogger.addHandler(log_handler)
+        syslogger.info(log_message)
+
+    # write to a log file
+    log_date = strftime(DATE_FORMAT, gmtime())
+    log_line = "{} - {}".format(log_date, log_message)
+    log_filename = "{}{}".format(LOG_DIR, strftime(NAME_FORMAT, gmtime()))
+    os.makedirs(os.path.dirname(log_filename), exist_ok=True)
+    with open(log_filename, "a") as f:
+        f.write("\n" + log_line)
